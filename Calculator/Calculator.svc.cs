a7d2447aae742bc2b5cd511368ee87822ae100c9
User <nishanth.thinksonic@gmail.com>
2019-03-25 12:49:08 +0530

    first commit

---
+++ Calculator/Calculator.svc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Calculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Calculator" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Calculator.svc or Calculator.svc.cs at the Solution Explorer and start debugging.
+    public class Calculator : ICalculator
+    {
+        //public void dowork()
+        //{
+        //}
+        #region ICalculator Members
+
+        string ICalculator.add(int a, int b)
+        {
+            return "Computed Sum: " + (a + b).ToString();
+        }
+
+        string ICalculator.Sub(int a, int b)
+        {
+            if (a >= b)
+            {
+                return "Computed Sum: " + (a - b).ToString();
+            }
+            else
+            {
+                return "Computed Sum: " + (b - a).ToString();
+            }
+        }
+
+        public string Mul(int a, int b)
+        {
+            return "Computed Sum: " + (a * b).ToString();
+        }
+
+        public string Div(int a, int b)
+        {
+            if (a >= b)
+                try { return (a / b).ToString(); }
+                catch
+                { return 0.ToString(); }
+            else
+                return "Computed Sum: " + (b / a).ToString();
+        }
+        public string XMLData(string id)
+        {
+            return "Computed Sum: " + id;
+        }
+
+        public string JSONData(string id)
+        {
+            return "Computed Sum: " + id.ToString();
+        }
+
+        #endregion
+    }
+
+}
---
+++ Calculator/ICalculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace Calculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculator" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        // void DoWork();
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string XMLData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string JSONData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/add?a={a}&b={b}")]
+        string add(int a, int b);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/sub?a={a}&b={b}")]
+        string Sub(int a, int b);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/mul?a={a}&b={b}")]
+        string Mul(int a, int b);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/div?a={a}&b={b}")]
+        string Div(int a, int b);
+    }
+
+   
+       
+        
+   
+}
